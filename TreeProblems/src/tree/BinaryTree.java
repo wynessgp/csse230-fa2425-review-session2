e25a90f4194f6963fdb7384efa656c97c452d484
wynessgp <wynessgp@rose-hulman.edu>
2024-10-05 00:56:18 -0400

    Pushing practice exam problems + tests

---
+++ TreeProblems/src/tree/BinaryTree.java
@@ -0,0 +1,227 @@
+package tree;
+
+import java.util.ArrayList;
+
+public class BinaryTree {
+	
+	BinaryNode root;
+	
+	// I arbitrarily chose -1
+	// You can really use any number you want for a NULL_NODE,
+	// Just make sure it's negative (so it does not interfere with tests)
+	final BinaryNode NULL_NODE = new BinaryNode(-1); 
+	
+	public BinaryTree() {
+		root = NULL_NODE;
+	}
+	
+	/*
+	 * Given a binary tree, this method should return the total
+	 * number of occurrences of the digit '7' throughout the
+	 * entire tree. 
+	 * 
+	 * This must be implemented using tree recursion and should
+	 * run in O(n) time, where n is the number of nodes in the tree.
+	 * Hint: you can convert the numbers to strings.
+	 */
+	public int countSevensInTree() {
+		// TODO: Implement me!
+		return -1;
+	}
+	
+	/**
+	 * You may safely assume this will ALWAYS be called on a BST.
+	 * You may NOT assume the item will be in the tree.
+	 * 	- If this occurs, you SHOULD return the empty list.
+	 *  - The tree SHOULD be mutated even if the item is not found.
+	 *  - (You are carving a path of destruction)
+	 * 
+	 * As you traverse down to find the target item in this tree,
+	 * you will "destroy" every branch that you do not walk down.
+	 * 
+	 * You don't have to worry about moving any nodes up to replace
+	 * the deleted one, you can simply get rid of the subtree.
+	 * 
+	 * You are to report back all of the nodes you deleted via
+	 * the data the "root" of the subtree had.
+	 * 
+	 * So if I have a tree that looks like:
+	 * 
+	 *                 52
+	 *               /    \
+	 *              13    58
+	 *                   /  \
+	 *                  54   59
+	 *                 /  \
+	 *                53  55
+	 * 
+	 * And my target was 59, I would return an ArrayList like:
+	 * 	[13, 54]
+	 * Note that it does NOT include 54's children.
+	 * 
+	 * This should run in O(height(T)) - namely, you walk 1 path.
+	 * 
+	 * @param target:
+	 * 	the number you are to traverse to while destroying
+	 * 	the rest of the tree.
+	 */
+	public ArrayList<Integer> pathOfDestruction(int target) {
+		// TODO: Implement me!
+		return new ArrayList<>();
+	}
+	
+	/**
+	 * HARD PROBLEM - Read carefully!
+	 * 
+	 * This method will originally be called on a BST, though
+	 * the nodes I will ask you to insert may not necessarily
+	 * end in this tree being a BST.
+	 * 
+	 * In this problem, any time that you reach a NULL_NODE on
+	 * the original tree you start with, you will insert the
+	 * new nodes containing the data provided in nodesToInsert
+	 * in a "flying v" formation. 
+	 * 
+	 * The middle item of the AL will ALWAYS be your root node of this new subtree
+	 * 
+	 * As an example, if I were to give you the list
+	 * 	[14, 17, 22, 15, 23]
+	 * It would generate this structure:
+	 * 
+	 *                     22
+	 *                    /  \
+	 *                   17  15
+	 *                  /      \
+	 *                 14      23
+	 * 
+	 * If the list provided is not an odd length, place the
+	 * extra node on the left:
+	 * 	[1, 2, 3, 4, 5, 6]
+	 * 
+	 *                     4
+	 *                    / \
+	 *                   3   5
+	 *                  /     \
+	 *                 2       6
+	 *                /
+	 *               1
+	 *               
+	 * Also, note that the list I give you may not be sorted.
+	 * That's OK, but you must maintain the order that they are passed in.
+	 * 
+	 * So if I initially have the tree:
+	 * 
+	 *                    25
+	 *                   /  \
+	 *                  17   31
+	 *                 /  \
+	 *                21  23
+	 * 
+	 * And I give you the list: [1, 2]
+	 * The resulting tree would look like:
+	 * 
+	 *                          25
+	 *                      /         \
+	 *                 17                 31
+	 *             /        \            /   \
+	 *         21              23       2     2
+	 *        /  \            /  \     /     / 
+	 *       2    2          2    2   1     1
+	 *      /    /          /    /
+	 *     1    1          1    1
+	 * 
+	 * @param nodesToInsert
+	 * 	An ArrayList containing the nodes you are to insert
+	 *  in the event you reach a NULL_NODE in the original tree.
+	 *  You may assume this never includes a negative number
+	 *  (so your null nodes are safe)
+	 */
+	public void addFlyingVs(ArrayList<Integer> itemsInV) {
+		// TODO: Implement me!
+	}
+	
+	// ---------------- Testing Methods -------------------
+	
+	// Methods utilized by the tests:
+	public void insert(int item) {
+		root = root.insert(item);
+	}
+	
+	public int height() {
+		return root.height();
+	}
+	
+	public int size() {
+		return root.size();
+	}
+	
+	// This will give you an in-order traversal of the tree,
+	// if you would like to use it in order to debug.
+	@Override
+	public String toString() {
+		return this.toArrayList().toString();
+	}
+	
+	public ArrayList<Integer> toArrayList() {
+		ArrayList<Integer> list = new ArrayList<>();
+		root.toArrayList(list);
+		return list;
+	}
+	
+	// ------------------- BINARY NODE ----------------------
+	
+	public class BinaryNode {
+		public int data;
+		public BinaryNode left;
+		public BinaryNode right;
+		
+		public BinaryNode(int element) {
+			this.data = element;
+			this.left = NULL_NODE;
+			this.right = NULL_NODE;
+		}
+		
+		// TODO: Add your methods you may need here
+		
+		// ... method 1 ...
+		
+		// ----------- Testing Methods ---------------
+		
+		public BinaryNode insert(int element) {
+			if (this == NULL_NODE) {
+				return new BinaryNode(element);
+			} else if (element < data) {
+				left = left.insert(element);
+			} else if (element > data) {
+				right = right.insert(element);
+			} else {
+				// Duplicate case shouldn't happen (search tree), but who knows?
+			}
+			return this;
+		}
+		
+		public int height() {
+			if (this == NULL_NODE) {
+				return -1;
+			}
+			return Math.max(left.height(), right.height()) + 1;
+		}
+		
+		public int size() {
+			if (this == NULL_NODE) {
+				return 0;
+			}
+			return left.size() + right.size() + 1;
+		}
+		
+		public void toArrayList(ArrayList<Integer> list) {
+			if (this == NULL_NODE) {
+				return;
+			}
+			left.toArrayList(list);
+			list.add(this.data);
+			right.toArrayList(list);
+		}
+	}
+	
+}
---
+++ TreeProblems/src/tree/BinaryTreeTest.java
@@ -0,0 +1,184 @@
+package tree;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class BinaryTreeTest {
+	
+	private BinaryTree bt;
+	
+	@BeforeEach
+	void createTree() {
+		bt = new BinaryTree();
+	}
+
+	@Test
+	void testCountSevensInTree_emptyTree_shouldReturnZero() {
+		assertEquals(0, bt.countSevensInTree());
+	}
+	
+	@Test
+	void testCountSevensInTree_nonEmptyTreeWithNoSevens_shouldReturnZero() {
+		bt.insert(15);
+		bt.insert(24);
+		bt.insert(3);
+		bt.insert(12);
+		assertEquals(0, bt.countSevensInTree());
+	}
+	
+	@Test
+	void testCountSevensInTree_number7And17InTree_shouldReturnTwo() {
+		bt.insert(13);
+		bt.insert(7); // make sure we check both sides...
+		bt.insert(15);
+		bt.insert(16);
+		bt.insert(17);
+		assertEquals(2, bt.countSevensInTree());
+	}
+	
+	@Test
+	void testCountSevensInTree_number777And77And27InTreeAtFarEnds_shouldReturnSix() {
+		bt.insert(375);
+		bt.insert(250);
+		bt.insert(125);
+		bt.insert(50);
+		bt.insert(27);
+		bt.insert(77);
+		bt.insert(14);
+		bt.insert(500);
+		bt.insert(625);
+		bt.insert(680);
+		bt.insert(654);
+		bt.insert(692);
+		bt.insert(777);
+		assertEquals(6, bt.countSevensInTree());
+	}
+	
+	@Test
+	void testPathOfDestruction_emptyTree_expectEmptyResults() {
+		ArrayList<Integer> res = bt.pathOfDestruction(0);
+		assertEquals(0, bt.size());
+		assertTrue(res.isEmpty());
+		assertEquals("[]", bt.toString());
+	}
+	
+	@Test
+	void testPathOfDestruction_itemInTree_checkTreeSizeAndString() {
+		bt.insert(52);
+		bt.insert(13);
+		bt.insert(58);
+		bt.insert(54);
+		bt.insert(53);
+		bt.insert(55);
+		bt.insert(59);
+		
+		int[] burnedNodes = {13, 54};
+		ArrayList<Integer> res = bt.pathOfDestruction(59);
+		assertEquals(2, res.size());
+		
+		for (int i = 0; i < burnedNodes.length; i++) {
+			// I'm being a little lenient here with just checking that your solution
+			// contains these nodes. 
+			assertTrue(res.contains(burnedNodes[i]));
+		}
+		
+		assertEquals(3, bt.size());
+		assertEquals("[52, 58, 59]", bt.toString());
+	}
+	
+	@Test
+	void testPathOfDestruction_itemNotInTree_checkTreeSizeAndString() {
+		bt.insert(52);
+		bt.insert(13);
+		bt.insert(58);
+		bt.insert(54);
+		bt.insert(53);
+		bt.insert(55);
+		bt.insert(59);
+		
+		ArrayList<Integer> res = bt.pathOfDestruction(17);
+		// Right subtree gets burned.
+		assertTrue(res.isEmpty());
+		
+		assertEquals(2, bt.size());
+		assertEquals("[13, 52]", bt.toString());
+	}
+	
+	@Test
+	void testAddFlyingVs_emptyTree_emptyItemsInV_expectEmptyTree() {
+		bt.addFlyingVs(new ArrayList<Integer>());
+		assertEquals(0, bt.size());
+		assertEquals("[]", bt.toString());
+	}
+	
+	@Test
+	void testAddFlyingVs_emptyTree_nonEmptyItemsInVEvenSize_expectFlyingVForm() {
+		ArrayList<Integer> itemsInV = (ArrayList<Integer>) List.of(1, 2, 3, 4);
+		bt.addFlyingVs(itemsInV);
+		assertEquals(4, bt.size());
+		assertEquals("[1, 2, 3, 4]", bt.toString());
+		// a bit of a draconian way to test it
+		assertEquals(3, bt.root.data);
+		assertEquals(2, bt.root.left.data);
+		assertEquals(4, bt.root.right.data);
+		assertEquals(1, bt.root.left.left.data);
+	}
+	
+	@Test
+	void testAddFlyingVs_emptyTree_nonEmptyItemsInVOddSize_expectFlyingVForm() {
+		ArrayList<Integer> itemsInV = (ArrayList<Integer>) List.of(2, 3, 4);
+		bt.addFlyingVs(itemsInV);
+		assertEquals(3, bt.size());
+		assertEquals("[2, 3, 4]", bt.toString());
+		// a bit of a draconian way to test it
+		assertEquals(3, bt.root.data);
+		assertEquals(2, bt.root.left.data);
+		assertEquals(4, bt.root.right.data);
+	}
+	
+	@Test
+	void testAddFlyingVs_nonEmptyTree_emptyFlyingVs_expectSameTree() {
+		bt.insert(5);
+		bt.insert(3);
+		bt.insert(7);
+		bt.insert(10);
+		
+		bt.addFlyingVs(new ArrayList<>());
+		assertEquals(4, bt.size());
+		assertEquals("[3, 5, 7, 10]", bt.toString());
+	}
+	
+	@Test
+	void testAddFlyingVs_nonEmptyTree_nonEmptyFlyingVsEvenSizeItems_expectSizeAndStringChanges() {
+		bt.insert(25);
+		bt.insert(17);
+		bt.insert(31);
+		bt.insert(21);
+		bt.insert(23);
+		
+		ArrayList<Integer> itemsInV = (ArrayList<Integer>) List.of(1, 2);
+		bt.addFlyingVs(itemsInV);
+		assertEquals(17, bt.size());
+		assertEquals("[1, 2, 21, 1, 2, 17, 1, 2, 23, 1, 2, 25, 1, 2, 31, 1, 2]", bt.toString());
+	}
+	
+	@Test
+	void testAddFlyingVs_nonEmptyTree_nonEmptyFlyingVsOddSizeItems_expectSizeAndStringChanges() {
+		bt.insert(25);
+		bt.insert(17);
+		bt.insert(31);
+		bt.insert(21);
+		bt.insert(23);
+		
+		ArrayList<Integer> itemsInV = (ArrayList<Integer>) List.of(1, 2, 3);
+		bt.addFlyingVs(itemsInV);
+		assertEquals(23, bt.size());
+		assertEquals("[1, 2, 3, 21, 1, 2, 3, 17, 1, 2, 3, 23, 1, 2, 3, 25, 1, 2, 3, 31, 1, 2, 3]", bt.toString());
+	}
+
+}
